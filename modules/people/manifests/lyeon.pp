b908ff9c4b284d370c5cff75d2c644b196459f41
Zach Ahn <zach.ahn@gmail.com>
2014-03-20 04:18:39 -0400

    comment things out

--- modules/people/manifests/lyeon.pp
+++ modules/people/manifests/lyeon.pp
@@ -1,16 +1,18 @@
 class people::lyeon {
-  include chrome
-  include skype
-  include dropbox
+  # include chrome
+  # include skype
+  # include dropbox
 
-  include github_for_mac
-  include sublime_text
+  # include github_for_mac
+  # include sublime_text
 
-  include osx::disable_app_quarantine
-  include osx::global::disable_key_press_and_hold
-  include osx::dock::autohide
-  include osx::dock::position
-  include osx::dock::icon_size
+  # include osx::disable_app_quarantine
+  # include osx::global::disable_key_press_and_hold
+  # include osx::dock::autohide
+  # include osx::dock::position
+  # include osx::dock::icon_size
+
+  include nodejs::global
 
   $home = "/Users/${::boxen_user}"
 }
