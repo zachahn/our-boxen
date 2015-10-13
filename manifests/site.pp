c77246dba94bc64108684ac6b5da63635e164414
Zach Ahn <zach.ahn@gmail.com>
2015-10-13 14:12:30 -0400

    don't install old rubies

--- manifests/site.pp
+++ manifests/site.pp
@@ -69,11 +69,12 @@ node default {
   # nodejs::version { '0.10': }
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+  # ruby::version { '1.9.3': }
+  # ruby::version { '2.0.0': }
+  # ruby::version { '2.1.0': }
+  # ruby::version { '2.1.1': }
+  # ruby::version { '2.1.2': }
+  ruby::version { '2.2.2': }
 
   # common, useful packages
   package {
